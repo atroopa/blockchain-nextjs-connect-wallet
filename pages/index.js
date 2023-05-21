2da399c4e0749bfcac81de685699e138851bbd5a
atroopa <atroopa@gmail.com>
2023-05-21 20:51:23 +0330

    coded use states

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import React, {useState, useEffect} from "react";
 import web3Modal from 'web3modal';
 import {ethers} from 'ethers';
+import imageEth from '../ether.png';
+import creator from '../creator.png';
 
 const Home = () => {
+
+  const [currentAccount, setCurrentAccount] = useState('');
+  const [connect, setConnect]               = useState(false);
+  const [balance , setBalance]              = useState('');
+
   return (
     <div>
       <h1>Home</h1>
